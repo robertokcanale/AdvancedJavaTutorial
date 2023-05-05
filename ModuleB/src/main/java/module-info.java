9ed38e2e2bc1f5d195c4a583dc5384b5be00df27
Roberto Canale <58591956+robertokcanale@users.noreply.github.com>
2023-05-05 08:14:55 +0800

    Chapter 5init, creaed modules

---
+++ ModuleB/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.moduleb {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.moduleb to javafx.fxml;
+    exports com.example.moduleb;
+}
---
+++ src/Chapter5/HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package Chapter5.HelloWorld;
+
+//Making the package modular and define how it relates yo my other modules
+//This Works at both PACKAGE LEVEL as well as MODULE LEVEL
+public class HelloWorld {
+    public static void main(String[] args) {
+            System.out.println("Hello World");
+    }
+    }
