de8ab0d9f01c17cbdb7f24ddb78af59b2488da80
Roberto Canale <58591956+robertokcanale@users.noreply.github.com>
2023-05-08 06:01:00 +0800

    Refactor just to avoid build issue

--- src/ModuleB/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.moduleb {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.moduleb to javafx.fxml;
-    exports com.example.moduleb;
-}
